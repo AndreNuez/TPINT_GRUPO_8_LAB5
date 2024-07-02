d43fb4fa7881f38961cf17f6ae4bdd7f61b55b77
RoberRang <rob_rang@hotmail.com>
2024-07-02 05:31:23 -0300

    Dao Jornada
    Interface de dao jornada y implantación

---
+++ src/daoImpl/daoJornada.java
@@ -0,0 +1,34 @@
+package daoImpl;
+
+import java.util.ArrayList;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import dao.IDaoJornada;
+import entidad.Jornada;
+
+public class daoJornada implements IDaoJornada {
+
+	@Autowired
+	private Conexion conexion;
+
+	@Override
+	public ArrayList<Jornada> readAll(int estado) {
+		
+		Session session = conexion.abrirConexion();
+		ArrayList<Jornada> jornadas = null;
+		String query = "FROM Jornadas j";			
+		query = estado == 0?" WHERE j.estado = 0":" WHERE j.estado = 1";			
+		try {			
+			
+			session.beginTransaction();
+			@SuppressWarnings({ "unchecked" })			
+			ArrayList<Jornada> jorns = (ArrayList<Jornada>)session.createQuery(query).list();
+			if (jorns.size()>0)
+				jornadas = jorns;
+			conexion.cerrarConexion();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return jornadas;
+	}
+}
