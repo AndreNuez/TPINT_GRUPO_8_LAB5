7d4429be16b8e77183b6968e5e07bb636fea6da1
Gabriel Vargas <ecgabrielec@hotmail.com>
2024-07-12 23:28:13 -0300

    Resolucion de conflictos

--- src/controller/ControladorNav.java
+++ src/controller/ControladorNav.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-	package controller;
+package controller;
 	
 	import java.util.ArrayList;
 	import java.util.List;
@@ -94,81 +93,6 @@ import entidad.Turno;
 	        return turnosFiltrados;
 	    }
 	}
-=======
-package controller;
 
-import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import entidad.Especialidad;
-import entidad.Medico;
-import entidad.Paciente;
-import entidad.Turno;
-import negocioImpl.MedicoNegocio;
-import negocioImpl.PacienteNegocio;
-import negocioImpl.TurnoNegocio;
-@Controller
-public class ControladorNav {
-	
-	@Autowired
-	@Qualifier("servicioMedico")
-	private MedicoNegocio medicoNg;
-
-	@Autowired
-	@Qualifier("servicioPaciente")
-	private PacienteNegocio pacienteNg;
-	
-	 @Autowired
-	 @Qualifier("servicioTurno")
-	 private TurnoNegocio turnoNg;
-
-    @RequestMapping("navPages.do")
-    public ModelAndView eventoRedireccionarPages(String inicioJsp, String medicosJsp, String pacientesJsp,
-            String turnosJsp, String reporteTurnosJsp, HttpSession session) {
-        ModelAndView MV = new ModelAndView();
-
-        if (inicioJsp != null && inicioJsp.equals("Inicio"))
-            MV.setViewName("Principal");
-        else if (medicosJsp != null && medicosJsp.equals("Medicos")) {
-        	List<Medico> medicos = medicoNg.ReadAll();
-        	MV.addObject("medicos", medicos);
-            MV.setViewName("ListarMedicos");
-           
-
-        } else if (pacientesJsp != null && pacientesJsp.equals("Pacientes")) {
-	    	List<Paciente> pacientes = pacienteNg.ReadAll();
-	    	MV.addObject("pacientes", pacientes);
-            MV.setViewName("ListarPacientes");
-            if (session.getAttribute("paciente") != null)
-    			session.setAttribute("paciente", null);
-
-	    }
-        
-        else if (turnosJsp != null && turnosJsp.equals("Turnos")) {
-            List<Turno> turnos = turnoNg.ReadAll();
-            MV.addObject("turnos", turnos);
-            MV.setViewName("ListarTurnos");
-        }
-        
-        else if (reporteTurnosJsp != null && reporteTurnosJsp.equals("ReporteTurnos")) {
-            MV.setViewName("ReporteTurnos");
-        }
-        return MV;
-    }
-
-    @RequestMapping("navLogOut.do")
-    public ModelAndView eventoRedireccionarLogOut(HttpSession session) {
-        ModelAndView MV = new ModelAndView();
-        MV.setViewName("Login");
-        return MV;
-    }
-}
->>>>>>> bdacaceddbf74b1e18ebce7a60d28e10170f18b4
 
