093e9ad1eb877b00a6c2d4961c88cfde472f1a1b
tfdace <58187350+tfdace@users.noreply.github.com>
2024-07-14 18:18:20 +0100

    Update ControladorNav.java

--- src/controller/ControladorNav.java
+++ src/controller/ControladorNav.java
@@ -1,43 +1,43 @@
 package controller;
 	
-	import java.util.ArrayList;
-	import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 	
-	import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 	
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.beans.factory.annotation.Qualifier;
-	import org.springframework.stereotype.Controller;
-	import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 	
-	import entidad.Medico;
-	import entidad.Paciente;
+import entidad.Medico;
+import entidad.Paciente;
 import entidad.PerfilUsuario;
 import entidad.Turno;
-	import entidad.Usuario;
-	import negocioImpl.MedicoNegocio;
-	import negocioImpl.PacienteNegocio;
-	import negocioImpl.TurnoNegocio;
-	@Controller
-	public class ControladorNav {
-		
-		@Autowired
-		@Qualifier("servicioMedico")
-		private MedicoNegocio medicoNg;
+import entidad.Usuario;
+import negocioImpl.MedicoNegocio;
+import negocioImpl.PacienteNegocio;
+import negocioImpl.TurnoNegocio;
+@Controller
+public class ControladorNav {
 	
-		@Autowired
-		@Qualifier("servicioPaciente")
-		private PacienteNegocio pacienteNg;
+	@Autowired
+	@Qualifier("servicioMedico")
+	private MedicoNegocio medicoNg;
+	
+	@Autowired
+	@Qualifier("servicioPaciente")
+	private PacienteNegocio pacienteNg;
 		
-		 @Autowired
-		 @Qualifier("servicioTurno")
-		 private TurnoNegocio turnoNg;
+	@Autowired
+	@Qualifier("servicioTurno")
+	private TurnoNegocio turnoNg;
 		 
 	
-	    @RequestMapping("navPages.do")
-	    public ModelAndView eventoRedireccionarPages(String inicioJsp, String medicosJsp, String pacientesJsp,
-	            String turnosJsp, String reporteTurnosJsp, HttpSession session) {
+	 @RequestMapping("navPages.do")
+	 public ModelAndView eventoRedireccionarPages(String inicioJsp, String medicosJsp, String pacientesJsp,
+	            String turnosJsp, String reporteTurnosJsp, String reporteEspecialidadesJsp, HttpSession session) {
 	        ModelAndView MV = new ModelAndView();
 	        Usuario user = (Usuario) session.getAttribute("user");
 	        
@@ -72,11 +72,17 @@ import entidad.Turno;
 	        	MV.addObject("exito", null);
 	        	MV.setViewName("ReporteTurnos");
 	        }
+	        
+	        else if (reporteEspecialidadesJsp != null && reporteEspecialidadesJsp.equals("ReporteEspecialidades")) {
+	        	MV.addObject("exito", null);
+	        	MV.setViewName("ReporteEspecialidades");
+	        }
+	        
 	        return MV;
 	    }
 	
-	    @RequestMapping("navLogOut.do")
-	    public ModelAndView eventoRedireccionarLogOut(HttpSession session) {
+	@RequestMapping("navLogOut.do")
+	public ModelAndView eventoRedireccionarLogOut(HttpSession session) {
 	        ModelAndView MV = new ModelAndView();
 	        MV.setViewName("Login");
 	        return MV;
@@ -92,8 +98,8 @@ import entidad.Turno;
 	        }
 	
 	        return turnosFiltrados;
-	    }
 	}
+}
 
 
 
